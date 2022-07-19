4f8a342b0759a2445383034de2074ee53d436b3b
David Eldar <doomtak@gmail.com>
2022-07-20 02:00:33 +0300

    footer changes

--- fiverrApp/src/cmps/app-footer.cmp.vue
+++ fiverrApp/src/cmps/app-footer.cmp.vue
@@ -6,20 +6,3 @@
 <<<<<<< HEAD
         &copy; Team D.A.D - Jul 2022
     </footer> -->
-=======
-        &copy; Team D.A.D - 2022
-    </footer>
-<<<<<<< HEAD
->>>>>>> 3a539bf4c79246bb2ee2a21a33d53279b8d92a12
-</template>
-=======
-</template> -->
->>>>>>> 3dbfa8c317b8ff95f26d29fa25ec062aa48c80e3
-
-<style>
-footer{
-    text-align: center;
-    display: grid;
-    justify-content:center;
-}
-</style>
