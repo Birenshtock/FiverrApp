97722cd2c39460ed07479f5ce75fa1c7f09bfd50
David Eldar <doomtak@gmail.com>
2022-07-19 23:26:24 +0300

    pull chainges

---
+++ fiverrApp/src/views/gig-app.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="gig-app">
+    
+    <!-- <gig-filter @setFilter="setFilter" /> -->
+    <!-- <gig-list :gigs="gigs" /> -->
+  </div>
+</template>
+
+<script>
+// import gigList from "../cmps/gig-list.cmp.vue";
+// import toyFilter from "../components/toy-filter.cmp.vue";
+export default {
+  template: ``,
+  components: {
+    // gigList,
+    // gigFilter,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+
+    // setFilter(filter) {
+    //   filter = JSON.parse(JSON.stringify(filter));
+    //   this.$store.dispatch({ type: "setFilterBy", filterBy: filter });
+    // },
+  },
+  computed: {
+    gigs() {
+      return this.$store.getters.gigsToDisplay;
+    },
+  },
+  created() {
+    
+  },
+  unmounted() {},
+};
+</script>
