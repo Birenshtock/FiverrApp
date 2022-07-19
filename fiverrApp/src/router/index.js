0ff554bc9ad17ff996d43aa43f40fef53ebe2bb6
danielleilyasov <danielle.ilyas@gmail.com>
2022-07-19 17:45:01 +0300

    daniel+adi  feat start proj with vue

---
+++ fiverrApp/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import home from "../views/home.vue"
+import about from "../views/about.vue"
+import gigApp from "../views/gig-app.vue"
+import gigDetails from "../views/gig-details.vue"
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: "/gig",
+      name: "gig-app",
+      component: gigApp,
+    },
+    {
+      path: "/gig/:gigId",
+      name: "gig-details",
+      component: gigDetails,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: about,
+    }
+  ]
+})
+
+export default router
