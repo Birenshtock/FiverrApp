abb968064f0229527dec09d95236d74633d058ee
David Eldar <doomtak@gmail.com>
2022-07-20 16:27:36 +0300

    header chainges

--- src/cmps/app-header.cmp.vue
+++ src/cmps/app-header.cmp.vue
@@ -1,11 +1,27 @@
 
 <template>
-  <header class="app-header">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/gig">Explore</router-link> |
-      <router-link to="/about">About</router-link> |
+  <header class="main-header">
+    <div class="header-container">
+      <div class="logo">Binderr<span>.</span></div>
+    <nav class="main-header-nav" ref="nav" v-bind:style="{ position: stickyNav ? 'fixed' : 'static' }">
+    <ul class="main-nav clean-list flex">
+      <li><router-link to="/">Home</router-link> |</li>
+      <li><router-link to="/gig">Explore</router-link> |</li>
+      <li><router-link to="/about">About</router-link> |</li>
+      </ul>
     </nav>
+    </div>
+    
   </header>
 </template>
 
+<script>
+export default {
+    data: () => {
+    return {
+
+      stickyNav: false,
+    };
+  },
+}
+</script>
